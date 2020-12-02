75f31d9a79c4353b9f7c8f2e34f34a47398925dc
Douglas Stebila <douglas@stebila.ca>
2020-12-02 10:00:37 -0500

    Use AWS CLI

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-rm -rf _site/benchmarking/visualization
-mkdir -p _site/benchmarking/visualization
-cd _site/benchmarking/visualization
-curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
+rm -rf _site/benchmarking/visualization_tmp
+mkdir -p _site/benchmarking/visualization_tmp
+cd _site/benchmarking/visualization_tmp
+# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
+aws s3 cp s3://oqs-tests/site/site.tgz .
 tar zxf site.tgz
 for f in *.list; do
     mv $f orig-$f
     tail orig-$f > $f
 done
+cd ..
+rm -rf visualization && mv visualization_tmp visualization

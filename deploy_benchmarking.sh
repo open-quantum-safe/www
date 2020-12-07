74b7b825538813f4a796cc35e3246f9aea15434e
Douglas Stebila <douglas@stebila.ca>
2020-12-07 13:22:49 -0500

    Fix path

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -6,7 +6,7 @@ rm -rf _site/benchmarking/visualization_tmp
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
 # curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
-aws s3 cp s3://oqs-tests/site/site.tgz .
+/usr/local/bin/aws s3 cp s3://oqs-tests/site/site.tgz .
 tar zxf site.tgz
 for f in *.list; do
     mv $f orig-$f

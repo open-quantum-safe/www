3608be3cac9a7ca447736564409f48f933356dbe
Douglas Stebila <douglas@stebila.ca>
2021-03-10 09:57:11 -0500

    Pull x86_64

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -6,8 +6,8 @@ rm -rf _site/benchmarking/visualization_tmp
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
 # curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
-/usr/local/bin/aws s3 cp s3://oqs-tests/site/site.tgz .
-tar zxf site.tgz
+/usr/local/bin/aws s3 cp s3://oqs-tests/site/site-x86_64.tgz .
+tar zxf site-x86_64.tgz
 for f in *.list; do
     mv $f orig-$f
     tail orig-$f > $f

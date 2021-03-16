c90355ccfc2b3987a9e83864dc0a89dc4001f453
Douglas Stebila <dstebila@uwaterloo.ca>
2021-03-16 15:59:03 -0400

    Put ARM and Intel onto a single landing page

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf _site/benchmarking/visualization_tmp
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
-# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
+# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-x86_64.tgz
 /usr/local/bin/aws s3 cp s3://oqs-tests/site/site-x86_64.tgz .
 tar zxf site-x86_64.tgz
 for f in *.list; do
@@ -13,10 +13,12 @@ for f in *.list; do
     tail orig-$f > $f
 done
 cd ..
+mkdir -p x86_64
 rm -rf x86_64/visualization && mv visualization_tmp x86_64/visualization
 cd ../..
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
+# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-aarch64.tgz
 /usr/local/bin/aws s3 cp s3://oqs-tests/site/site-aarch64.tgz .
 tar zxf site-aarch64.tgz
 for f in *.list; do
@@ -24,6 +26,7 @@ for f in *.list; do
     tail orig-$f > $f
 done
 cd ..
+mkdir -p aarch64
 rm -rf aarch64/visualization && mv visualization_tmp aarch64/visualization
 cd ../..
 

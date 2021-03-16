86d59c5ef0c5848cbedd54e0da56b9ada026a3e4
Michael Baentsch <info@baentsch.ch>
2021-03-16 21:01:32 +0100

    adding ARM benchmarking (#19)
    * adding ARM benchmarking
    
    * Put ARM and Intel onto a single landing page
    
    * Fix order
    
    * Fix metadata
    
    Co-authored-by: Douglas Stebila <dstebila@uwaterloo.ca>

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf _site/benchmarking/visualization_tmp
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
-# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site.tgz
+# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-x86_64.tgz
 /usr/local/bin/aws s3 cp s3://oqs-tests/site/site-x86_64.tgz .
 tar zxf site-x86_64.tgz
 for f in *.list; do
@@ -13,4 +13,20 @@ for f in *.list; do
     tail orig-$f > $f
 done
 cd ..
-rm -rf visualization && mv visualization_tmp visualization
+mkdir -p x86_64
+rm -rf x86_64/visualization && mv visualization_tmp x86_64/visualization
+cd ../..
+mkdir -p _site/benchmarking/visualization_tmp
+cd _site/benchmarking/visualization_tmp
+# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-aarch64.tgz
+/usr/local/bin/aws s3 cp s3://oqs-tests/site/site-aarch64.tgz .
+tar zxf site-aarch64.tgz
+for f in *.list; do
+    mv $f orig-$f
+    tail orig-$f > $f
+done
+cd ..
+mkdir -p aarch64
+rm -rf aarch64/visualization && mv visualization_tmp aarch64/visualization
+cd ../..
+

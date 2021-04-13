67f378b6e2e7f0a4f10de834596ccb89de0a0e37
Michael Baentsch <info@baentsch.ch>
2021-04-13 16:03:36 +0200

    multiarch visualization deployment (#25)

--- deploy_benchmarking.sh
+++ deploy_benchmarking.sh
@@ -5,28 +5,15 @@ set -e
 rm -rf _site/benchmarking/visualization_tmp
 mkdir -p _site/benchmarking/visualization_tmp
 cd _site/benchmarking/visualization_tmp
-# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-x86_64.tgz
-/usr/local/bin/aws s3 cp s3://oqs-tests/site/site-x86_64.tgz .
-tar zxf site-x86_64.tgz
-for f in *.list; do
-    mv $f orig-$f
-    tail orig-$f > $f
-done
-cd ..
-mkdir -p x86_64
-rm -rf x86_64/visualization && mv visualization_tmp x86_64/visualization
-cd ../..
-mkdir -p _site/benchmarking/visualization_tmp
-cd _site/benchmarking/visualization_tmp
-# curl --silent -O https://oqs-tests.s3.us-east-2.amazonaws.com/site/site-aarch64.tgz
-/usr/local/bin/aws s3 cp s3://oqs-tests/site/site-aarch64.tgz .
-tar zxf site-aarch64.tgz
-for f in *.list; do
-    mv $f orig-$f
-    tail orig-$f > $f
-done
-cd ..
-mkdir -p aarch64
-rm -rf aarch64/visualization && mv visualization_tmp aarch64/visualization
-cd ../..
+/usr/local/bin/aws s3 cp s3://oqs-tests/site/site-combined.tgz .
+if [ $? -eq 0 ]; then
+   # The tarball only contains the last 10 days of data: No further processing required
+   tar zxf site-combined.tgz
+   cd ..
+   rm -rf visualization && mv visualization_tmp visualization
+   cd ../..
+   echo "New data successfully deployed"
+else
+   cd ../../..
+fi
 
